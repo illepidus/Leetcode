bdc3c1c12ea0367178ebc084167ee4e35927cc53
ivblinov <ivblinov@yandex-team.ru>
2023-02-24 00:18:33 +0300

    Problem 2

---
+++ src/main/java/ru/krotarnya/leetcode/problem0002/ListNode.java
@@ -0,0 +1,12 @@
+package ru.krotarnya.leetcode.problem0002;
+
+/**
+ * @author ivblinov
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
