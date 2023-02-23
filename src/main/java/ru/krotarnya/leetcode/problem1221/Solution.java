924508a5490d2df5097f90669733afda03c36e97
ivblinov <ivblinov@yandex-team.ru>
2023-02-24 02:18:57 +0300

    Problem 1221

---
+++ src/main/java/ru/krotarnya/leetcode/problem1221/Solution.java
@@ -0,0 +1,20 @@
+package ru.krotarnya.leetcode.problem1221;
+
+/**
+ * <p>Balanced strings are those that have an equal quantity of 'L' and 'R' characters.</p>
+ * <p>Given a balanced string s, split it into some number of substrings such that:</p>
+ * <p>    Each substring is balanced.</p>
+ * <p>Return the maximum number of balanced strings you can obtain.</p>
+ */
+public class Solution {
+    public static int balancedStringSplit(String s) {
+        int balance = 0;
+        int result = 0;
+        for (char c: s.toCharArray()) {
+            if (c == 'L') balance--;
+            if (c == 'R') balance++;
+            if (balance == 0) result++;
+        }
+        return result;
+    }
+}
