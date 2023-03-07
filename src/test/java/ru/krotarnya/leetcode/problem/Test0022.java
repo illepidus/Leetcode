7fc3485bcfa8d019217c9a2e3b2155eb7555ed9b
illepidus <illepidus@gmail.com>
2023-03-08 02:44:44 +0300

    Problem 22

---
+++ src/test/java/ru/krotarnya/leetcode/problem/Test0022.java
@@ -0,0 +1,50 @@
+package ru.krotarnya.leetcode.problem;
+
+import java.util.List;
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static ru.krotarnya.leetcode.problem.p0022.Solution.generateParenthesisPrecalculated;
+import static ru.krotarnya.leetcode.problem.p0022.Solution.generateParenthesisRecursive;
+import static ru.krotarnya.leetcode.problem.p0022.Solution.validateParenthesis;
+
+public class Test0022 {
+    @ValueSource(strings = {"", "()", "()()", "(())"})
+    @ParameterizedTest
+    void validateParenthesisTestTrue(String s) {
+        assertTrue(validateParenthesis(s));
+    }
+
+    @ValueSource(strings = {"(", ")", "((", ")(", "))", "((()", ")()("})
+    @ParameterizedTest
+    void validateParenthesisTestFalse(String s) {
+        assertFalse(validateParenthesis(s));
+    }
+
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6, 7, 8})
+    @ParameterizedTest
+    void generateParenthesisRecursiveTest(int n) {
+        List<String> result = generateParenthesisRecursive(n);
+        assertEquals(result.size(), Set.copyOf(result).size());
+        result.forEach(s -> assertTrue(validateParenthesis(s)));
+    }
+
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6, 7, 8})
+    @ParameterizedTest
+    void generateParenthesisPrecalculatedTest(int n) {
+        assertEquals(generateParenthesisRecursive(n), generateParenthesisPrecalculated(n));
+    }
+    
+    @Test
+    void generateStatic() {
+        for (int i = 1; i <= 8; i++) {
+            System.out.printf("            %d, List.of(\"%s\"),\n", i, String.join("\", \"", generateParenthesisRecursive(i)));
+        }
+    }
+}
