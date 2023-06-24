ba93bfe4428297ed4c07e540941feeb2a25bd0d8
illepidus <illepidus@gmail.com>
2023-06-24 19:07:57 +0300

    print-in-order

---
+++ src/main/java/ru/krotarnya/leetcode/problem/p1114/Foo.java
@@ -0,0 +1,35 @@
+package ru.krotarnya.leetcode.problem.p1114;
+
+import java.util.concurrent.CyclicBarrier;
+
+import ru.krotarnya.leetcode.Complexity;
+import ru.krotarnya.leetcode.Problem;
+
+@Problem(id = 1114, name = "print-in-order", complexity = Complexity.EASY)
+public class Foo {
+    private final CyclicBarrier firstBarrier;
+    private final CyclicBarrier secondBarrier;
+    public Foo() {
+        firstBarrier = new CyclicBarrier(2);
+        secondBarrier = new CyclicBarrier(2);
+    }
+    public void first(Runnable r) {
+        try {
+            r.run();
+            firstBarrier.await();
+        } catch (Exception ignored) {}
+    }
+    public void second(Runnable r) {
+        try {
+            firstBarrier.await();
+            r.run();
+            secondBarrier.await();        
+        } catch (Exception ignored) {}
+    }
+    public void third(Runnable r) {
+        try {
+            secondBarrier.await();
+            r.run();
+        } catch (Exception ignored) {}
+    }
+}
---
+++ src/test/java/ru/krotarnya/leetcode/problem/Test1114.java
@@ -0,0 +1,30 @@
+package ru.krotarnya.leetcode.problem;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import ru.krotarnya.leetcode.problem.p1114.Foo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Test1114 {
+    private static final Foo FOO = new Foo();
+    private static final List<Integer> RESULT = new ArrayList<>();
+    private static final Map<Integer, Runnable> MAP = Map.of(
+            1, () -> FOO.first(() -> RESULT.add(1)),
+            2, () -> FOO.second(() -> RESULT.add(2)),
+            3, () -> FOO.third(() -> RESULT.add(3))
+    );
+    
+    @CsvSource({"1, 2, 3", "1, 3, 2", "2, 1, 3", "2, 3, 1", "3, 1, 2", "3, 2, 1"})
+    @ParameterizedTest
+    void test(int _1, int _2, int _3) throws InterruptedException {
+        RESULT.clear();
+        List.of(_1, _2, _3).forEach(i -> new Thread(MAP.get(i)).start());
+        Thread.sleep(10);
+        assertEquals(List.of(1, 2, 3), RESULT);
+    }
+}
